71cea27b9cd9ce00acdfbc23368eea74058b6f77
Junji Hashimoto <junji.hashimoto@gree.net>
2019-05-17 19:12:52 +0900

    Fix bug

--- download-binary.sh
+++ download-binary.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 
+#USE_NIGHTLY="nightly/"
+USE_NIGHTLY=""
+
 for arch in "shared-with-deps-latest" "macos-latest" "win-shared-with-deps-latest" ; do
 		for cuda in cpu cu80 cu90 cu100 ; do
 				if [ $arch = "macos-latest" ] && [ $cuda = "cu90" -o $cuda = "cu100" -o $cuda = "cu80" ]; then
 					 echo "skip ${cuda} of ${arch}";
 				else
-						wget -O ${cuda}-libtorch-${arch}.zip https://download.pytorch.org/libtorch/nightly/${cuda}/libtorch-${arch}.zip;
+						wget -O ${cuda}-libtorch-${arch}.zip https://download.pytorch.org/libtorch/${USE_NIGHTLY}${cuda}/libtorch-${arch}.zip;
 				fi
 		done
 done
--- upload-binary.sh
+++ upload-binary.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 VERSION=`cat libtorch/build-version`
+github-release release -u hasktorch -r libtorch-binary-for-ci -t $VERSION
 for i in *.zip  ; do
 		echo $i;
 		github-release upload -u hasktorch -r libtorch-binary-for-ci -t $VERSION -R -n $i -f $i ;
